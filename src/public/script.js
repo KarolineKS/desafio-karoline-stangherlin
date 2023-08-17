c16f3b93a0b2620e7db53ec068850393141aacb1
Karoline <kkohlsstangherlin@gmail.com>
2023-08-16 23:45:20 -0300

    :rocket: [feat] Página está funcionando

--- src/public/script.js
+++ src/public/script.js
@@ -1,10 +1,22 @@
 import { Cardapio } from '../cardapio-da-lanchonete.js';
+import { CaixaDaLanchonete } from '../caixa-da-lanchonete.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const cardapio = new Cardapio();
   const cardapioItems = cardapio.gerarCardapio;
 
   const itemsList = document.querySelector('.items__list');
+  const btnmetodoDePagamento = document.querySelector('.metodo-pagamento');
+  const valorTotalElement = document.querySelector('.total__price');
+  const somarButton = document.querySelector('.btn__somar');
+  const cartItemsList = document.querySelector('.cart__items');
+  const limparButton = document.querySelector('.btn__limpar');
+
+  let metodoDePagamento = btnmetodoDePagamento.value;
+
+  btnmetodoDePagamento.addEventListener('change', () => {
+    metodoDePagamento = btnmetodoDePagamento.value;
+  });
 
   const urlImages = {
     cafe: 'https://static.vecteezy.com/system/resources/previews/009/887/134/original/cup-of-coffee-free-png.png',
@@ -14,20 +26,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
     suco: 'https://img.freepik.com/fotos-premium/copo-de-suco-de-laranja-e-laranjas_159938-2735.jpg',
 
-    sanduiche: "https://img.freepik.com/fotos-gratis/sanduiche_1339-1108.jpg?w=360",
+    sanduiche:
+      'https://img.freepik.com/fotos-gratis/sanduiche_1339-1108.jpg?w=360',
 
-    queijo: "https://www.pngmart.com/files/8/Cheese-PNG-Transparent-Images.png",
+    queijo: 'https://www.pngmart.com/files/8/Cheese-PNG-Transparent-Images.png',
 
-    salgado: "https://salgadosneves.com.br/img/salgado_fumaca.png",
+    salgado: 'https://salgadosneves.com.br/img/salgado_fumaca.png',
 
-    combo1: "https://portofit.files.wordpress.com/2012/07/sanduiche.jpg",
+    combo1: 'https://portofit.files.wordpress.com/2012/07/sanduiche.jpg',
 
-    combo2: "https://img.freepik.com/fotos-premium/sanduiche-de-caranguejo-presunto-e-cafe-em-fundo-branco_44045-70.jpg",
+    combo2:
+      'https://img.freepik.com/fotos-premium/sanduiche-de-caranguejo-presunto-e-cafe-em-fundo-branco_44045-70.jpg',
   };
 
   cardapioItems.forEach((item) => {
     const itemCard = createItemCard(item);
+    itemCard.querySelector('.btn__add').addEventListener('click', () => {
+      addItemToCart(item);
+    });
     itemsList.appendChild(itemCard);
+  });
+
+  somarButton.addEventListener('click', () => {
+    calcularValor();
+  });
+
+  limparButton.addEventListener('click', () => {
+    limparCarrinho();
   });
 
   function createItemCard(item) {
@@ -61,5 +86,77 @@ document.addEventListener('DOMContentLoaded', () => {
     card.appendChild(addButton);
 
     return card;
+  }
+
+  function addItemToCart(item) {
+    const cartItem = createCartItem(item);
+    cartItemsList.appendChild(cartItem);
+  }
+
+  function createCartItem(item) {
+    const cartItem = document.createElement('li');
+    cartItem.classList.add('cart__item');
+
+    const image = document.createElement('img');
+    image.classList.add('cart__item__image');
+    image.src = urlImages[item.codigo];
+    cartItem.appendChild(image);
+
+    const description = document.createElement('span');
+    description.classList.add('cart__item__description');
+    description.textContent = item.codigo;
+    cartItem.appendChild(description);
+
+    const quantity = document.createElement('span');
+    quantity.classList.add('cart__item__quantity');
+    quantity.textContent = `Quant: 1`;
+    cartItem.appendChild(quantity);
+
+    const price = document.createElement('span');
+    price.classList.add('cart__item__price');
+    price.textContent = `Preço: R$ ${item.valor.toFixed(2)}`;
+    cartItem.appendChild(price);
+
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('btn__remove');
+    removeButton.textContent = 'X';
+    removeButton.addEventListener('click', () => {
+      cartItem.remove();
+    });
+    cartItem.appendChild(removeButton);
+
+    return cartItem;
+  }
+
+  function calcularValor() {
+    const cartItens = document.querySelectorAll('.cart__item');
+    const itens = Array.from(cartItens).map((item) => {
+      const codigo = item.querySelector('.cart__item__description').textContent;
+      const quantidadeText = item.querySelector(
+        '.cart__item__quantity'
+      ).textContent;
+      const quantidade = Number(quantidadeText.split(' ')[1]);
+      const itensFormados = `${codigo}, ${quantidade}`;
+      return itensFormados;
+    });
+    const formatedItens = [...itens];
+    console.log('formatedItens', formatedItens);
+
+    const caixaDaLanchonete = new CaixaDaLanchonete();
+    const valorTotal = caixaDaLanchonete.calcularValorDaCompra(
+      metodoDePagamento,
+      itens
+    );
+
+    valorTotalElement.textContent = `${valorTotal}`;
+  }
+
+  function limparCarrinho() {
+    const cartItems = document.querySelectorAll('.cart__item');
+    cartItems.forEach((item) => {
+      item.remove();
+    });
+    cartItems.innerHTML = '';
+    valorTotalElement.textContent = 'R$ 0.00';
   }
 });
